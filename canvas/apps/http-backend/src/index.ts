7645c8101fcfa8f753a63e814a01b4d8256089df
Rishiikesh <rishiikeshsk@gmail.com>
2026-01-12 18:35:13 +0530

    fix: add build dependency to dev task and format JSON

--- canvas/apps/http-backend/src/index.ts
+++ canvas/apps/http-backend/src/index.ts
@@ -1,8 +1,19 @@
+import "./env.js";
+import cors from "cors";
 import express from "express";
+import { globalErrorHandler } from "./error/error.js";
+import { router } from "./routes/v1.js";
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
+
+app.use(globalErrorHandler);
+
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
