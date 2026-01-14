e733937f90670619ae18187a3a75559a53076e12
Rishiikesh <rishiikeshsk@gmail.com>
2026-01-14 13:23:26 +0530

    feat : resolve build error

--- canvas/apps/ws-backend/src/index.ts
+++ canvas/apps/ws-backend/src/index.ts
@@ -1,10 +1,9 @@
-const { WebSocketServer } = require("ws");
-// @ts-expect-error
-const { setupWSConnection } = require("y-websocket/bin/utils");
+import { WebSocketServer } from "ws";
+// @ts-expect-error - y-websocket doesn't have type definitions
+import { setupWSConnection } from "y-websocket/bin/utils";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-// @ts-expect-error
 wss.on("connection", (ws, req) => {
 	setupWSConnection(ws, req);
 });
